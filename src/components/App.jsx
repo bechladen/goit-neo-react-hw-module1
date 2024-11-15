7667de34fb054d078586b223b10e4f9a40c04f9c
Yurii Bech <bechladen@gmail.com>
2024-11-15 16:45:08 -0330

    demo

---
+++ src/components/App.jsx
@@ -0,0 +1,27 @@
+// src / App.jsx;
+
+import Product from './Product';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Products</h1>
+
+      <Product />
+      <Product />
+      <Product />
+    </div>
+  );
+}
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Best selling</h1>
+
+//       <Product />
+//       <Product />
+//       <Product />
+//     </div>
+//   );
+// }
